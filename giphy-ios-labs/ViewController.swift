8db53eed58258321adf84a16fc0ed3d1c79fb6f2
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-02-15 23:27:36 +0000

    Creates xcode project

---
+++ giphy-ios-labs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  giphy-ios-labs
+//
+//  Created by Filipe Jorge on 15/02/2020.
+//  Copyright © 2020 Filipe Jorge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
