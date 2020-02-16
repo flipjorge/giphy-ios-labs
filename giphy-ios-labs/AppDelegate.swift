9f0d3dd385fa13294dfb90893af429bd5d88d085
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-02-16 22:45:54 +0000

    Moves logic to the ViewController

--- giphy-ios-labs/AppDelegate.swift
+++ giphy-ios-labs/AppDelegate.swift
@@ -15,56 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // Getting GIPHY Key
-        guard let keysPath = Bundle.main.path(forResource: "Keys", ofType: "plist") else {
-            print("Keys.plist file with api keys is required in order to run the app")
-            return true
-        }
-        
-        let keysDictionary = NSDictionary(contentsOf: URL(fileURLWithPath: keysPath)) as! Dictionary<String, AnyObject>
-        
-        guard let giphyKey = keysDictionary["GIPHY"] else {
-            print("Can't find the GIPHY key")
-            return true
-        }
-        
-        //Request random gif
-        let urlSession = URLSession.shared
-        let url = URL(string: "https://api.giphy.com/v1/gifs/random?api_key=\(giphyKey)&tag=&rating=G")
-        urlSession.dataTask(with: url!) { data, response, error in
-            
-            //check error
-            if error != nil {
-                print(error!)
-                return
-            }
-            
-            //check response
-            guard let httpResponse = response as? HTTPURLResponse,
-                httpResponse.statusCode == 200 else
-            {
-                print("Status code for respose != 200")
-                return
-            }
-            
-            guard let mime = httpResponse.mimeType, mime == "application/json" else {
-                print("Not returning a json")
-                return
-            }
-            
-            //parse data
-            do
-            {
-                let decoder = JSONDecoder()
-                let giphyRandomData = try decoder.decode(GiphyRandom.self, from: data!)
-                
-                print (giphyRandomData.data.images.downsized_still.url)
-                
-            } catch {
-                print("Error parsing data: \(error.localizedDescription)")
-            }
-            
-        }.resume()
+        //
         
         return true
     }
--- giphy-ios-labs/ViewController.swift
+++ giphy-ios-labs/ViewController.swift
@@ -12,7 +12,57 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Getting GIPHY Key
+        guard let keysPath = Bundle.main.path(forResource: "Keys", ofType: "plist") else {
+            print("Keys.plist file with api keys is required in order to run the app")
+            return
+        }
+        
+        let keysDictionary = NSDictionary(contentsOf: URL(fileURLWithPath: keysPath)) as! Dictionary<String, AnyObject>
+        
+        guard let giphyKey = keysDictionary["GIPHY"] else {
+            print("Can't find the GIPHY key")
+            return
+        }
+        
+        //Request random gif
+        let urlSession = URLSession.shared
+        let url = URL(string: "https://api.giphy.com/v1/gifs/random?api_key=\(giphyKey)&tag=&rating=G")
+        urlSession.dataTask(with: url!) { data, response, error in
+            
+            //check error
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            //check response
+            guard let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200 else
+            {
+                print("Status code for respose != 200")
+                return
+            }
+            
+            guard let mime = httpResponse.mimeType, mime == "application/json" else {
+                print("Not returning a json")
+                return
+            }
+            
+            //parse data
+            do
+            {
+                let decoder = JSONDecoder()
+                let giphyRandomData = try decoder.decode(GiphyRandom.self, from: data!)
+                
+                print (giphyRandomData.data.images.downsized_still.url)
+                
+            } catch {
+                print("Error parsing data: \(error.localizedDescription)")
+            }
+            
+        }.resume()
     }
 
 
