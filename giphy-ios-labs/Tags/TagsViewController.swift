21eee8f5b5b0d6dd4341fb16c9ff6271f167175b
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-02-22 23:58:21 +0000

    Performs done when enter on keyboard

--- giphy-ios-labs/Tags/TagsViewController.swift
+++ giphy-ios-labs/Tags/TagsViewController.swift
@@ -38,6 +38,7 @@ extension TagsViewController : UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
     {
         textField.resignFirstResponder()
+        performSegue(withIdentifier: "tagsDone", sender: self)
         return true
     }
 }
