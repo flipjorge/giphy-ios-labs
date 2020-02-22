30863a5425aae7345807e4f47aed8e378140293e
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2020-02-22 22:19:02 +0000

    Creates TagsViewController

---
+++ giphy-ios-labs/Tags/TagsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TagsViewController.swift
+//  giphy-ios-labs
+//
+//  Created by Filipe Jorge on 22/02/2020.
+//  Copyright © 2020 Filipe Jorge. All rights reserved.
+//
+
+import UIKit
+
+class TagsViewController : UIViewController
+{
+    override func viewDidLoad() {
+        //
+    }
+}
